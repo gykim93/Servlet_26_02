e71b4f05cf3dd055a2a7b840880fec7161b6133d
gykim93 <gykim1993@gmail.com>
2026-02-23 20:09:53 +0900

    게시물 상세페이지 진행중

--- src/main/java/DB.sql
+++ src/main/java/DB.sql
@@ -1,23 +1,62 @@
+# 데이터베이스 생성
 DROP DATABASE IF EXISTS `AM_jsp_2026_02`;
 CREATE DATABASE `AM_jsp_2026_02`;
 USE `AM_jsp_2026_02`;
 
+
+# 게시글 테이블 생성
 CREATE TABLE `article` (
     `id` INT PRIMARY KEY AUTO_INCREMENT,
-    `title` CHAR(100),
-    `body` CHAR(100)
+    `regDate` DATETIME NOT NULL,
+    `updateDate` DATETIME NOT NULL,
+    `title` CHAR(100) NOT NULL,
+    `body` CHAR(100) NOT NULL
 );
 
-INSERT INTO `article`
-SET `title` = '제목1',
-    `body` = '내용1';
-INSERT INTO `article`
-SET `title` = '제목2',
-    `body` = '내용2';
-INSERT INTO `article`
-SET `title` = '제목3',
-    `body` = '내용3';    
+# 회원 테이블 생성
+CREATE TABLE `member`(
+	`id` INT PRIMARY KEY AUTO_INCREMENT,
+	`regDate` DATETIME NOT NULL,
+	`updateDate` DATETIME NOT NULL,
+	`loginId` CHAR(100) NOT NULL,
+	`loginPw` CHAR(100) NOT NULL,
+	`name` CHAR(100) NOT NULL
+);
 
+# 게시글 데이터 삽입
+INSERT INTO `article`
+SET `regDate` = NOW(),
+	`updateDate` = NOW(),
+	`title` = '제목1',
+	`body` = '내용1';
+	
+INSERT INTO `article`
+SET `regDate` = NOW(),
+	`updateDate` = NOW(),
+	`title` = '제목2',
+	`body` = '내용2';
+
+INSERT INTO `article`
+SET `regDate` = NOW(),
+	`updateDate` = NOW(),
+	`title` = '제목2',
+	`body` = '내용2';
+	
 SELECT * FROM `article`;
 
-
+# 회원 데이터 삽입
+INSERT INTO `member`
+SET `regDate` = NOW(),
+	`updateDate` = NOW(),
+	`loginId` = 'test1',
+	`loginPw` = 'test1',
+	`name` = '홍길동';
+	
+INSERT INTO `member`
+SET `regDate` = NOW(),
+	`updateDate` = NOW(),
+	`loginId` = 'test1',
+	`loginPw` = 'test1',
+	`name` = '홍길순';	
+	
+SELECT * FROM `member`;
---
+++ src/main/java/KoreaIT/ArticleDetailServlet.java
@@ -0,0 +1,66 @@
+package KoreaIT;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/article/list")
+public class ArticleDetailServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=UTF-8");
+
+		System.out.println(123);
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("클래스 없음");
+			e.printStackTrace();
+		}
+
+		String url = "jdbc:mysql://127.0.0.1:3306/AM_jsp_2026_02?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";
+		String user = "root";
+		String password = "";
+
+		Connection conn = null;
+
+		try {
+			conn = DriverManager.getConnection(url, user, password);
+
+			// DBUtil을 통해 데이터 가져오기
+			DBUtil dbUtil = new DBUtil(request, response);
+			String sql = "SELECT * FROM article;";
+			List<Map<String, Object>> articleRows = dbUtil.selectRows(conn, sql);
+
+			// JSP에서 사용할 수 있도록 데이터를 담기
+			request.setAttribute("articleRows", articleRows);
+
+			// list.jsp 화면으로 제어권을 넘기기. (포워딩)
+			request.getRequestDispatcher("/jsp/article/list.jsp").forward(request, response);
+
+		} catch (SQLException e) {
+			System.out.println("에러 : " + e);
+		} finally {
+			try {
+				if (conn != null && !conn.isClosed()) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
