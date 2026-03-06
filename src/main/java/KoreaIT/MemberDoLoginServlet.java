02064e4e341f2bf7d4c4fcda785243463e81d21c
gykim93 <gykim1993@gmail.com>
2026-03-06 20:54:52 +0900

    로그인 기능 구현 완료, 로그아웃 구현 전

---
+++ src/main/java/KoreaIT/MemberDoLoginServlet.java
@@ -0,0 +1,90 @@
+package KoreaIT;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Map;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/member/doLogin")
+public class MemberDoLoginServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=UTF-8");
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("클래스 없음");
+			e.printStackTrace();
+		}
+
+		String url = "jdbc:mysql://127.0.0.1:3306/AM_jsp_2026_02?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";
+		String user = "root";
+		String password = "";
+
+		Connection conn = null;
+
+		try {
+			conn = DriverManager.getConnection(url, user, password);
+
+			String loginId = request.getParameter("loginId");
+			String loginPw = request.getParameter("loginPw");
+
+			SecSql sql = SecSql.from("SELECT *");
+			sql.append("FROM `member`");
+			sql.append("WHERE loginId = ?;", loginId);
+
+			Map<String, Object> memberRow = DBUtil.selectRow(conn, sql);
+
+			System.out.println(memberRow);
+
+			if (memberRow.isEmpty()) {
+				response.getWriter().append(String
+						.format("<script>alert('%s는 없는 회원'); location.replace('../member/login');</script>", loginId));
+				return;
+			}
+
+			if (memberRow.get("loginPw").equals(loginPw) == false) {
+				response.getWriter().append(String.format(
+						"<script>alert('패스워드가 틀렸습니다.'); location.replace('../member/login');</script>", loginId));
+				return;
+			}
+			// 로그인 기능 구현중 핵심.
+			HttpSession session = request.getSession();
+			session.setAttribute("loginedMember", memberRow);
+			session.setAttribute("loginedMemberId", memberRow.get("id"));
+			session.setAttribute("loginedMemberLoginId", memberRow.get("loginId"));
+
+			response.getWriter()
+					.append(String.format("<script>alert('%s님 로그인 성공!'); location.replace('../article/list');</script>",
+							memberRow.get("name")));
+
+		} catch (SQLException e) {
+			System.out.println("에러 : " + e);
+		} finally {
+			try {
+				if (conn != null && !conn.isClosed()) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
